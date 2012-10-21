4fd77743e81459ce843a656e9870309b02e09966
Ryham <Ryham@hotmail.com>
2012-10-22 00:29:14 +0200

    inclusion xml

--- FileParser/src/Exemple.java
+++ FileParser/src/Exemple.java
@@ -4,11 +4,16 @@ import java.io.File;
 public class Exemple {
 	public static void main(String args[])
 	{
-		CountHandler ct = new CountHandler();
+		CountHandler ct1 = new CountHandler();
+		CountHandler ct2 = new CountHandler();
 		File fl = new File("monFichierTest.txt");
+		File f2 = new File("fichier.xml");
 		FileParser fp = new FileParser();
-		fp.parse(fl,ct);
-		System.out.print("le fichier contient " + ct.getCompL()+" lignes");
-		System.out.print(" dont " + ct.getCmpC() + " sont des commentaires");
+		fp.parse(fl,ct1);
+		fp.parse(f2,ct2);
+		System.out.print("le fichier contient " + ct1.getCompL()+" lignes");
+		System.out.print(" dont " + ct1.getCmpC() + " sont des commentaires");
+		System.out.print("le fichier contient " + ct2.getCompL()+" lignes");
+		System.out.print(" dont " + ct2.getCmpC() + " sont des commentaires");
 	}
 }
--- FileParser/src/FileParser.java
+++ FileParser/src/FileParser.java
@@ -1,34 +1,42 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.Reader;
 
 
 public class FileParser{
 	public void parse(File file, ContentHandler handler)
 	{
+		boolean trouve_xml = false;
 		try{
 			InputStream ips=new FileInputStream(file); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
 			String ligne;
-			while ((ligne=br.readLine())!=null){
-				if(ligne.startsWith("#"))
+			if ((ligne=br.readLine())!=null){
+				if(ligne.startsWith("<?xml"))
 				{
-					handler.commentLine(ligne);
-					handler.defaultLine(ligne);
+					while ((ligne=br.readLine())!=null)
+					{
+						if(ligne.endsWith("-->"))
+							trouve_xml = false;
+						if(ligne.startsWith("<!--"))
+							trouve_xml =true;
+						if(trouve_xml==false)
+						{
+							handler.defaultLine(ligne);
+						}
+						else
+						{
+							handler.defaultLine(ligne);
+							handler.commentLine(ligne);
+						}
+					}
+					br.close(); 
 				}
-				else
-					handler.defaultLine(ligne);
 			}
-			br.close(); 
-		}		
+		}
 		catch (Exception e){
 			System.out.println(e.toString());
 		}
